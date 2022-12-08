356728846763542643f629afd789f74f9e4e3795
David Sadler <djs21@ic.ac.uk>
2022-12-08 20:43:33 +0000

    Added status flag macros

--- src/cpu.h
+++ src/cpu.h
@@ -2,6 +2,15 @@
 
 #include <stdint.h>
 
+#define STATUS(S, C) ((S >> C) & (uint8_t)1)
+#define NEGATIVE(S)     STATUS(S, 7)
+#define OVERFLOW(S)     STATUS(S, 6)
+#define BREAK(S)        STATUS(S, 4)
+#define DECIMAL(S)      STATUS(S, 3)
+#define INTR_DISABLE(S) STATUS(S, 2)
+#define ZERO(S)         STATUS(S, 1)
+#define CARRY(S)        (S & (uint8_t)1)
+
 struct registers {
   uint8_t accumulator;
   uint8_t x;
