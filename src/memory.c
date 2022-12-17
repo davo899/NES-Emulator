43bd5c40b6817dff9e81065fb72acb7d3f981616
David Sadler <djs21@ic.ac.uk>
2022-12-17 02:31:27 +0000

    Deleted memory.c/h

--- src/memory.c
+++
@@ -1,7 +0,0 @@
-#include "memory.h"
-
-uint8_t *memory;
-
-void init_memory(int bytes) {
-  memory = calloc(bytes, 1);
-}
--- src/memory.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-extern uint8_t *memory;
-
-void init_memory(int bytes);
