03d3fcd7c0e8c9a60e962cab965be24663bd6f52
David Sadler <djs21@ic.ac.uk>
2022-12-17 17:10:02 +0000

    Fixed test_bytes_equal message not printing

--- src/instructions.c
+++ src/instructions.c
@@ -208,3 +208,92 @@ static void SED(enum addressing_mode addressing_mode, struct registers *register
 static void SEI(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {
   SET(INTR_DISABLE_FLAG, registers->status);
 }
+
+
+static void ADC(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void AND(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void ASL(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+
+static void BCC(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void BCS(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void BEQ(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void BIT(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void BMI(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void BNE(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void BPL(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void BRK(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void BVC(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void BVS(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+
+static void CMP(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void CPX(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void CPY(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+
+static void DEC(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void DEX(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void DEY(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+
+static void EOR(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+
+static void INC(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void INX(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void INY(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+
+static void JMP(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void JSR(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+
+static void LDA(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void LDX(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void LDY(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void LSR(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+
+static void NOP(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+
+static void ORA(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+
+static void PHA(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void PHP(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void PLA(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void PLP(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+
+static void ROL(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void ROR(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void RTI(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void RTS(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+
+static void SBC(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void STA(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void STX(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void STY(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+
+// Illegal Opcode Instructions
+static void ALR(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void ANC(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void ANE(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void ARR(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+
+static void DCP(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+
+static void ISC(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+
+static void JAM(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+
+static void LAS(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void LAX(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void LXA(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+
+static void RLA(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void RRA(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+
+static void SAX(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void SBX(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void SHA(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void SHX(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void SHY(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void SLO(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+static void SRE(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+
+static void TAS(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
+
+//Unsafe SBC
+static void UBC(enum addressing_mode addressing_mode, struct registers *registers, uint8_t *memory) {}
