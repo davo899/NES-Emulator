5c300f3bfe0baf80bdc61ee77568d178b4aba492
David Sadler <djs21@ic.ac.uk>
2022-12-28 17:33:27 +0000

    Made ROM writes be ignored instead of causing a crash

--- src/main.c
+++ src/main.c
@@ -16,14 +16,13 @@ static uint8_t NES_read(uint16_t address) {
   if      (address < 0x2000) return ram[address & 0b0000011111111111];
   else if (address < 0x4000) return ppu_read(ppu, address & 0b0000000000000111);
   else if (address < 0x4014) return apu_read(apu, address & 0b0000000000001111);
-  else                       return rom[address - 0x4020];
+  else                       return rom[address - 0x8000];
 }
 
 static void NES_write(uint8_t data, uint16_t address) {
   if      (address < 0x2000) ram[address & 0b0000011111111111] = data;
   else if (address < 0x4000) ppu_write(ppu, address & 0b0000000000000111, data);
   else if (address < 0x4014) apu_write(apu, address & 0b0000000000001111, data);
-  else                       error("Attempted to write to ROM");
 }
 
 int main(int argc, char *argv[]) {
